b2cf5ecdc67b39d2875030f69305836250f16ec9
LydiaMarieWilliamson <60720435+LydiaMarieWilliamson@users.noreply.github.com>
2021-09-15 02:24:44 -0500

    Translation to C: round 2G.
    Removed duplicate definitions of common blocks from common.c.
    Added in the data files as symbolic links (for now).
    Compile-time baseline established - the program compiles.
    No run-time baseline has yet been established - the compiled program crashes.

--- C/common.c
+++ C/common.c
@@ -1,5 +1,22 @@
 #include "F2C.h"
 
+#if 0
+// Already defined in blkdata.c:
+// buzvoc:
+struct {
+   int bvoc[20];
+} buzvoc_;
+
+// prpvoc:
+struct {
+   int pvoc[45];
+} prpvoc_;
+
+// dirvoc:
+struct {
+   int dvoc[75];
+} dirvoc_;
+
 // adjvoc:
 union {
    struct {
@@ -10,10 +27,99 @@ union {
    } _2;
 } adjvoc_;
 
-// advs:
+// vrbvoc:
+union {
+   struct {
+      int vvoc1[92], vvoc1a[108], vvoc1b[38], vvoc2[104], vvoc3[136], vvoc4[116], vvoc5[134], vvoc6[117], vvoc7[89], vvocnd;
+   } _1;
+   struct {
+      double eqv_pad[475];
+   } _2;
+} vrbvoc_;
+
+// objvoc:
+union {
+   struct {
+      int ovoc1[159], ovoc2[144], ovoc3[150], ovoc4[128], ovoc5[111], ovoc6[104], ovoc6a[97], ovoc7[127], ovocnd;
+   } _1;
+   struct {
+      double eqv_pad[525];
+   } _2;
+} objvoc_;
+#endif
+
+#if 0
+// Already defined in dmain.c:
+// synflg:
 struct {
-   int alnt, aroom[4], ascore[4], avehic[4], aobj[4], aactio[4], astren[4], aflag[4];
-} advs_;
+   int sdir, sind, sstd, sflip, sdriv, svmask;
+} synflg_;
+
+// objflg:
+struct {
+   int vabit, vrbit, vtbit, vcbit, vebit, vfbit, vpmask;
+} objflg_;
+
+// screen:
+struct {
+   int fromdr, scolrm, scolac, scoldr[8], scolwl[12];
+} screen_;
+
+// puzzle:
+struct {
+   int cpdr[16], cpwl[8], cpvec[64];
+} puzzle_;
+
+// vers:
+struct {
+   int vmaj, vmin;
+   char vedit[1];
+} vers_;
+
+// bats:
+struct {
+   int batdrp[9];
+} bats_;
+
+// hyper:
+struct {
+   int hfactr;
+} hyper_;
+
+// rflag:
+struct {
+   int rseen, rlight, rland, rwater, rair, rsacrd, rfill, rmung, rbuck, rhouse, rnwall, rend;
+} rflag_;
+
+// rindex:
+struct {
+   int whous, lroom, cella, mtrol, maze1, mgrat, maz15, fore1, fore3, clear, reser, strea, egypt, echor, tshaf, bshaf, mmach, dome, mtorc, carou, riddl, lld2, temp1, temp2, maint, blroo, treas, rivr1, rivr2, rivr3, mcycl, rivr4, rivr5, fchmp, falls, mbarr, mrain, pog, vlbot, vair1, vair2, vair3, vair4, ledg2, ledg3, ledg4, msafe, cager, caged, twell, bwell, alice, alism, alitr, mtree, bkent, bkvw, bktwi, bkvau, bkbox, crypt, tstrs, mrant, mreye, mra, mrb, mrc, mrg, mrd, fdoor, mrae, mrce, mrcw, mrge, mrgw, mrdw, inmir, scorr, ncorr, parap, cell, pcell, ncell, cpant, cpout, cpuzz;
+} rindex_;
+
+// xpars:
+struct {
+   int xrmask, xdmask, xfmask, xfshft, xashft, xelnt[4], xnorm, xno, xcond, xdoor, xlflag;
+} xpars_;
+
+// xsrch:
+struct {
+   int xmin, xmax, xdown, xup, xnorth, xsouth, xenter, xexit, xeast, xwest;
+} xsrch_;
+
+// oflags:
+struct {
+   int visibt, readbt, takebt, doorbt, tranbt, foodbt, ndscbt, drnkbt, contbt, litebt, victbt, burnbt, flambt, toolbt, turnbt, onbt, findbt, slepbt, scrdbt, tiebt, clmbbt, actrbt, weapbt, fitebt, villbt, stagbt, trybt, nochbt, openbt, tchbt, vehbt, schbt;
+} oflags_;
+
+// oindex:
+struct {
+   int garli, food, gunk, coal, machi, diamo, tcase, bottl, water, rope, knife, sword, lamp, blamp, rug, leave, troll, axe, rknif, keys, ice, bar, coffi, torch, tbask, fbask, irbox, ghost, trunk, bell, book, candl, match, tube, putty, wrenc, screw, cyclo, chali, thief, still, windo, grate, door, hpole, leak, rbutt, raili, pot, statu, iboat, dboat, pump, rboat, stick, buoy, shove, ballo, recep, guano, brope, hook1, hook2, safe, sslot, brick, fuse, gnome, blabe, dball, tomb, lcase, cage, rcage, spher, sqbut, flask, pool, saffr, bucke, ecake, orice, rdice, blice, robot, ftree, bills, portr, scol, zgnom, egg, begg, baubl, canar, bcana, ylwal, rdwal, pindr, rbeam, odoor, qdoor, cdoor, num1, num8, warni, cslit, gcard, stldr, hands, wall, lungs, sailo, aviat, teeth, itobj, every, valua, oplay, wnort, gwate, master;
+} oindex_;
+
+// cindex:
+struct {
+   int cevcur, cevmnt, cevlnt, cevmat, cevcnd, cevbal, cevbrn, cevfus, cevled, cevsaf, cevvlg, cevgno, cevbuc, cevsph, cevegh, cevfor, cevscl, cevzgi, cevzgo, cevste, cevmrs, cevpin, cevinq, cevfol;
+} cindex_;
 
 // aflags:
 struct {
@@ -25,15 +131,16 @@ struct {
    int player, arobot, amastr;
 } aindex_;
 
-// bats:
+// vindex:
 struct {
-   int batdrp[9];
-} bats_;
+   int cintw, deadxw, frstqw, inxw, outxw, walkiw, fightw, foow, meltw, readw, inflaw, deflaw, alarmw, exorcw, plugw, kickw, wavew, raisew, lowerw, rubw, pushw, untiew, tiew, tieupw, turnw, breatw, knockw, lookw, examiw, shakew, movew, trnonw, trnofw, openw, closew, findw, waitw, spinw, boardw, unboaw, takew, invenw, fillw, eatw, drinkw, burnw, mungw, killw, attacw, swingw, walkw, tellw, putw, dropw, givew, pourw, throww, digw, leapw, stayw, follow, hellow, lookiw, lookuw, pumpw, windw, clmbw, clmbuw, clmbdw, trntow;
+} vindex_;
+#endif
 
-// buzvoc:
+// advs:
 struct {
-   int bvoc[20];
-} buzvoc_;
+   int alnt, aroom[4], ascore[4], avehic[4], aobj[4], aactio[4], astren[4], aflag[4];
+} advs_;
 
 // cevent:
 struct {
@@ -46,11 +153,6 @@ struct {
    int inpch, outch, dbch;
 } chan_;
 
-// cindex:
-struct {
-   int cevcur, cevmnt, cevlnt, cevmat, cevcnd, cevbal, cevbrn, cevfus, cevled, cevsaf, cevvlg, cevgno, cevbuc, cevsph, cevegh, cevfor, cevscl, cevzgi, cevzgo, cevste, cevmrs, cevpin, cevinq, cevfol;
-} cindex_;
-
 // curxt:
 struct {
    int xtype, xroom1, xstrng, xactio, xobj;
@@ -60,11 +162,6 @@ struct {
 struct {
    int dbgflg, prsflg, gdtflg;
 } debug_;
-
-// dirvoc:
-struct {
-   int dvoc[75];
-} dirvoc_;
 
 // exits:
 struct {
@@ -84,11 +181,6 @@ struct {
    int swdsta;
 } hack_;
 
-// hyper:
-struct {
-   int hfactr;
-} hyper_;
-
 // input:
 struct {
    int inlnt;
@@ -106,31 +198,6 @@ struct {
    , oroom[220], oadv[220], ocan[220], oread[220];
 } objcts_;
 
-// objflg:
-struct {
-   int vabit, vrbit, vtbit, vcbit, vebit, vfbit, vpmask;
-} objflg_;
-
-// objvoc:
-union {
-   struct {
-      int ovoc1[159], ovoc2[144], ovoc3[150], ovoc4[128], ovoc5[111], ovoc6[104], ovoc6a[97], ovoc7[127], ovocnd;
-   } _1;
-   struct {
-      double eqv_pad[525];
-   } _2;
-} objvoc_;
-
-// oflags:
-struct {
-   int visibt, readbt, takebt, doorbt, tranbt, foodbt, ndscbt, drnkbt, contbt, litebt, victbt, burnbt, flambt, toolbt, turnbt, onbt, findbt, slepbt, scrdbt, tiebt, clmbbt, actrbt, weapbt, fitebt, villbt, stagbt, trybt, nochbt, openbt, tchbt, vehbt, schbt;
-} oflags_;
-
-// oindex:
-struct {
-   int garli, food, gunk, coal, machi, diamo, tcase, bottl, water, rope, knife, sword, lamp, blamp, rug, leave, troll, axe, rknif, keys, ice, bar, coffi, torch, tbask, fbask, irbox, ghost, trunk, bell, book, candl, match, tube, putty, wrenc, screw, cyclo, chali, thief, still, windo, grate, door, hpole, leak, rbutt, raili, pot, statu, iboat, dboat, pump, rboat, stick, buoy, shove, ballo, recep, guano, brope, hook1, hook2, safe, sslot, brick, fuse, gnome, blabe, dball, tomb, lcase, cage, rcage, spher, sqbut, flask, pool, saffr, bucke, ecake, orice, rdice, blice, robot, ftree, bills, portr, scol, zgnom, egg, begg, baubl, canar, bcana, ylwal, rdwal, pindr, rbeam, odoor, qdoor, cdoor, num1, num8, warni, cslit, gcard, stldr, hands, wall, lungs, sailo, aviat, teeth, itobj, every, valua, oplay, wnort, gwate, master;
-} oindex_;
-
 // oroom2:
 struct {
    int r2lnt, oroom2[20], rroom2[20];
@@ -147,22 +214,12 @@ struct {
    Bool telflg;
 } play_;
 
-// prpvoc:
-struct {
-   int pvoc[45];
-} prpvoc_;
-
 // prsvec:
 struct {
    int prsa, prsi, prso;
    Bool prswon;
    int prscon;
 } prsvec_;
-
-// puzzle:
-struct {
-   int cpdr[16], cpwl[8], cpvec[64];
-} puzzle_;
 
 // pv:
 struct {
@@ -174,16 +231,6 @@ struct {
    int seedy;
 } random_;
 
-// rflag:
-struct {
-   int rseen, rlight, rland, rwater, rair, rsacrd, rfill, rmung, rbuck, rhouse, rnwall, rend;
-} rflag_;
-
-// rindex:
-struct {
-   int whous, lroom, cella, mtrol, maze1, mgrat, maz15, fore1, fore3, clear, reser, strea, egypt, echor, tshaf, bshaf, mmach, dome, mtorc, carou, riddl, lld2, temp1, temp2, maint, blroo, treas, rivr1, rivr2, rivr3, mcycl, rivr4, rivr5, fchmp, falls, mbarr, mrain, pog, vlbot, vair1, vair2, vair3, vair4, ledg2, ledg3, ledg4, msafe, cager, caged, twell, bwell, alice, alism, alitr, mtree, bkent, bkvw, bktwi, bkvau, bkbox, crypt, tstrs, mrant, mreye, mra, mrb, mrc, mrg, mrd, fdoor, mrae, mrce, mrcw, mrge, mrgw, mrdw, inmir, scorr, ncorr, parap, cell, pcell, ncell, cpant, cpout, cpuzz;
-} rindex_;
-
 // rmsg:
 struct {
    int mlnt, rtext[1820];
@@ -193,11 +240,6 @@ struct {
 struct {
    int rlnt, rdesc2, rdesc1[200], rexit[200], ractio[200], rval[200], rflag[200];
 } rooms_;
-
-// screen:
-struct {
-   int fromdr, scolrm, scolac, scoldr[8], scolwl[12];
-} screen_;
 
 // star:
 struct {
@@ -209,11 +251,6 @@ struct {
    int moves, deaths, rwscor, mxscor, mxload, ltshft, bloc, mungrm, hs, egscor, egmxsc;
 } state_;
 
-// synflg:
-struct {
-   int sdir, sind, sstd, sflip, sdriv, svmask;
-} synflg_;
-
 // syntax:
 struct {
    int vflag, dobj, dfl1, dfl2, dfw1, dfw2, iobj, ifl1, ifl2, ifw1, ifw2;
@@ -224,38 +261,7 @@ struct {
    int pltime, shour, smin, ssec;
 } time_;
 
-// vers:
-struct {
-   int vmaj, vmin;
-   char vedit[1];
-} vers_;
-
 // vill:
 struct {
    int vlnt, villns[4], vprob[4], vopps[4], vbest[4], vmelee[4];
 } vill_;
-
-// vindex:
-struct {
-   int cintw, deadxw, frstqw, inxw, outxw, walkiw, fightw, foow, meltw, readw, inflaw, deflaw, alarmw, exorcw, plugw, kickw, wavew, raisew, lowerw, rubw, pushw, untiew, tiew, tieupw, turnw, breatw, knockw, lookw, examiw, shakew, movew, trnonw, trnofw, openw, closew, findw, waitw, spinw, boardw, unboaw, takew, invenw, fillw, eatw, drinkw, burnw, mungw, killw, attacw, swingw, walkw, tellw, putw, dropw, givew, pourw, throww, digw, leapw, stayw, follow, hellow, lookiw, lookuw, pumpw, windw, clmbw, clmbuw, clmbdw, trntow;
-} vindex_;
-
-// vrbvoc:
-union {
-   struct {
-      int vvoc1[92], vvoc1a[108], vvoc1b[38], vvoc2[104], vvoc3[136], vvoc4[116], vvoc5[134], vvoc6[117], vvoc7[89], vvocnd;
-   } _1;
-   struct {
-      double eqv_pad[475];
-   } _2;
-} vrbvoc_;
-
-// xpars:
-struct {
-   int xrmask, xdmask, xfmask, xfshft, xashft, xelnt[4], xnorm, xno, xcond, xdoor, xlflag;
-} xpars_;
-
-// xsrch:
-struct {
-   int xmin, xmax, xdown, xup, xnorth, xsouth, xenter, xexit, xeast, xwest;
-} xsrch_;
