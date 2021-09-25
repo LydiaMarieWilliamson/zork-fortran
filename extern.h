d63550e5e6ec0fca7aacc885c222cb411fb6d02d
LydiaMarieWilliamson <60720435+LydiaMarieWilliamson@users.noreply.github.com>
2021-09-25 16:32:30 -0500

    Merger of F2C.h into extern.h.

---
+++ extern.h
@@ -0,0 +1,166 @@
+// Copyright (c) 1980, InfoCom Computers and Communications, Cambridge MA 02142
+// All rights reserved, commercial usage strictly prohibited.
+// Written by R. M. Supnik.
+// Revisions Copyright (c) 2021, Darth Spectra (Lydia Marie Williamson).
+
+// The functions for dungeon.
+
+// The remnant of f2c.h (begin)
+// Assumes C99, which is now (well into the 21st century) POSIX.
+// That means: boolean types, function prototypes, ‟rb” and ‟wb” for fopen(), ‟const”, ‟void”, etc.
+#include <stdbool.h>
+#include <stdlib.h>
+
+// This type will be nativized to ‟bool”, after it's made OK to do so.
+typedef int Bool; // Logical.
+
+#define abs(X)		((X) >= 0? (X): -(X))
+#define min(A, B)	((A) <= (B)? (A): (B))
+#define max(A, B)	((A) >= (B)? (A): (B))
+
+// local.c:
+// A wrapper for the built-in functions defined in the f2c library.
+int DoFio(size_t N, void *X, size_t dN);
+int BegInSF(int Unit, char *Format);
+int EndInSF(void);
+int BegExSF(int Unit, char *Format);
+int EndExSF(void);
+// The remnant of f2c.h (end)
+
+// actors.c:
+Bool aappli(int);
+void thiefd(void);
+
+// ballop.c:
+Bool ballop(int);
+
+// clockr.c:
+void cevapp(int);
+
+// demons.c:
+void fightd(void);
+int blow(int, int, int, Bool, int);
+void swordd(void);
+
+// dgame.c:
+void game_(void);
+
+// dinit.c:
+Bool init(void/*int*/);
+
+// dso1.c:
+void princr(Bool, int);
+void invent(int);
+void princo(int, int);
+
+// dso2.c:
+Bool moveto(int, int);
+void score(Bool);
+void scrupd(int);
+
+// dso3.c:
+Bool findxt(int, int);
+int fwim(int, int, int, int, int, Bool);
+Bool yesno(int, int, int);
+
+// dso4.c:
+int robadv(int, int, int, int);
+int robrm(int, int, int, int, int);
+Bool winnin(int, int);
+int fights(int, Bool);
+int vilstr(int);
+
+// dso5.c:
+int gttime(void);
+Bool opncls(int, int, int);
+Bool lit(int);
+int weight(int, int, int);
+
+// dso6.c:
+Bool ghere(int, int);
+int mrhere(int);
+
+// dso7.c:
+void encryp(const char *, char * /*, size_t, size_t*/);
+void cpgoto(int);
+void cpinfo(int, int);
+
+// dsub.c:
+void rspeak(int);
+void rspsub(int, int);
+void rspsb2(int, int, int);
+Bool objact(void/*int*/);
+void bug(int, int);
+void newsta(int, int, int, int, int);
+Bool qhere(int, int);
+Bool qempty(int);
+void jigsup(int);
+int oactor(int);
+Bool prob(int, int);
+Bool rmdesc(int);
+Bool rappli(int);
+
+// dverb1.c:
+Bool take(Bool);
+Bool drop(void/*Bool*/);
+Bool put(void/*Bool*/);
+void valuac(int);
+
+// dverb2.c:
+void savegm(void);
+void rstrgm(void);
+Bool walk(void/*int*/);
+
+// gdt.c:
+void gdt(void);
+
+// lightp.c:
+Bool lightp(int);
+
+// nobjs.c:
+Bool nobjs(int/*, int*/);
+
+// np1.c:
+int sparse(int *, int, Bool);
+
+// np2.c:
+int getobj(int, int, int);
+int schlst(int, int, int, int, int, int);
+
+// np3.c:
+Bool synmch(void/*int*/);
+
+// np.c:
+void rdline(char *, int, int, size_t);
+Bool parse(char *, int, Bool/*, size_t*/);
+void orphan(int, int, int, int, int);
+
+// nrooms.c:
+Bool rappl2(int);
+
+// objcts.c:
+Bool oappli(int, int);
+
+// rooms.c:
+Bool rappl1(int);
+
+// rtim.c:
+void intime(int *, int *, int *);
+void inirnd(int);
+int rnd(int);
+void exit_(void);
+
+// sobjs.c:
+Bool sobjs(int, int);
+
+// sverbs.c:
+Bool sverbs(int);
+
+// verbs.c:
+Bool vappli(int);
+Bool clockd(void/*int*/);
+
+// villns.c:
+Bool trollp(void/*int*/);
+Bool cyclop(void/*int*/);
+Bool thiefp(void/*int*/);
---
+++ local.c
@@ -0,0 +1,48 @@
+// Revisions Copyright (c) 2021, Darth Spectra (Lydia Marie Williamson).
+#include "extern.h"
+#include <string.h>
+
+// A wrapper for the built-in functions defined in the f2c library.
+// I/O stuff.
+#ifdef f2c_i2
+// for -i2
+typedef short Bit, SizeT, Int;
+#else
+typedef long Bit, SizeT, Int;
+#endif
+
+int DoFio(size_t N, void *X, size_t dN) {
+   int do_fio(int *, char *, SizeT);
+   int n = (int)N; char *x = (char *)X; SizeT dn = (SizeT)dN;
+   return do_fio(&n, x, dn);
+}
+
+typedef struct {
+   Bit _Err;
+   Int _Unit;
+   Bit _End;
+   char *_Format;
+   Int _Rec;
+} cilist;
+
+int BegInSF(int Unit, char *Format) {
+   int s_rsfe(cilist *);
+   cilist L = (cilist){_Err: (Bit)false, _Unit: (Int)Unit, _End: (Bit)false, _Format: Format, _Rec: (Int)0};
+   return s_rsfe(&L);
+}
+
+int EndInSF(void) {
+   int e_rsfe(void);
+   return e_rsfe();
+}
+
+int BegExSF(int Unit, char *Format) {
+   int s_wsfe(cilist *);
+   cilist L = (cilist){_Err: (Bit)false, _Unit: (Int)Unit, _End: (Bit)false, _Format: Format, _Rec: (Int)0};
+   return s_wsfe(&L);
+}
+
+int EndExSF(void) {
+   int e_wsfe(void);
+   return e_wsfe();
+}
