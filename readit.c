733cb1b4408c9fd1d4c9e12c6aefd2153ae793ac
LydiaMarieWilliamson <60720435+LydiaMarieWilliamson@users.noreply.github.com>
2021-09-26 14:10:52 -0500

    Removing size-dependency from readit.c.
    There are no portable size-independent types in C99: uint16_t and int16_t are optional, not mandatory.

--- readit.c
+++ readit.c
@@ -1,23 +1,20 @@
 #include <stdio.h>
-#include <stdint.h>
 
 struct frec {
-   int_least16_t indx;
-   char c[74];
+   char ix[2], buf[74];
 } rec;
 
 int main(void) {
    FILE *fp = fopen("dtext.dat", "rb");
    int x = 0;
-   char buf[75]; buf[74] = '\0';
+   size_t n = sizeof rec.buf;
+   char buf[n + 1]; buf[n] = '\0';
    while (fread(&rec, sizeof rec, 1, fp) == 1) {
       x++;
-      for (int i = 1; i < 75; i++) {
-         int mask = (x&31) + i;
-         buf[i - 1] = rec.c[i - 1] ^ mask;
-      }
-      for (char *cp = &buf[73]; *cp == ' '; cp--) *cp = '\0';
-      printf("%4d %4d: %s\n", x, (int)rec.indx, buf);
+      for (int i = 0; i < n; i++) buf[i] = rec.buf[i] ^ ((x&31) + i + 1);
+      for (char *cp = &buf[n - 1]; *cp == ' '; cp--) *cp = '\0';
+      unsigned indx = (unsigned)(rec.ix[0]&0xff) | (unsigned)(rec.ix[1]&0xff) << 8;
+      printf("%4d %4u: %s\n", x, indx, buf);
    }
    return 0;
 }
