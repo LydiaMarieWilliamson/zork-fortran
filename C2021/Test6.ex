d54455d4bae4f4c67396ebdfb88d03fff8d5fb86
LydiaMarieWilliamson <60720435+LydiaMarieWilliamson@users.noreply.github.com>
2021-10-26 14:32:37 -0500

    More test files added.
    ... the first of many.

---
+++ C2021/Test6.ex
@@ -0,0 +1,17 @@
+Welcome to Dungeon.   This version created 11-FEB-87.
+You are in an open field west of a big white house with a boarded
+front door.
+There is a small mailbox here.
+>Nice weather we've been having lately.
+>Good day.
+>You can't take the mailbox.
+>Opening the mailbox reveals:
+  A leaflet.
+>I can't see one here.
+>Taken.
+>I don't think that the leaflet would agree with you.
+>Closed.
+>Your score would be 0 [total of 585 points], in 9 moves.
+This gives you the rank of Beginner.
+Do you wish to leave the game?
+Game Over.
